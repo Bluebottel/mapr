8bc3c700267dada8002e56cbb5223cb6e3b363ef
Saskian <bluebottel@gmail.com>
2021-08-28 21:11:19 +0200

    Filterbar coding style updated

--- src/filterpanel.js
+++ src/filterpanel.js
@@ -2,22 +2,22 @@ import React from 'react'
 
 import './styles/filterpanel.css'
 
-const Filterpanel = props => {
+function Filterpanel({ filterString, setFilterString }) {
 
-  return (
-    <div className = 'filter-panel'>
-      <input
-	type = 'text'
-	className = 'filter-bar'
-	placeholder = 'Filter by boat or owner name, equipment'
-	value = { props.filterString }
-	onChange = { event => {
-	  props.setFilterString(event.target.value)
-	}}
-	onFocus = { event => event.target.select() }
-      />
-    </div>
-  )
-}	  
+	return (
+		<div className = 'filter-panel'>
+			<input
+				type = 'text'
+				className = 'filter-bar'
+				placeholder = 'Filter on title'
+				value = { filterString }
+				onChange = { event => {
+					setFilterString(event.target.value)
+				}}
+				onFocus = { event => event.target.select() }
+			/>
+		</div>
+	)
+}
 
 export default Filterpanel
